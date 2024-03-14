ceb0c9469beb486b7d9c06273180a02677423a99
allenryder <rajoli.rahul@gmail.com>
2024-03-14 10:45:23 +0530

    my first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = ()=>{
+    return(
+        <div className="container">
+            <div  className="title">
+            Plan A Tour
+            </div>
+        </div>
+    )
+}
+
+export default Header;
